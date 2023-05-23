1b1f4176ca47e8dedbe75c181550f631ee168df4
GlebSats <133129364+GlebSats@users.noreply.github.com>
2023-05-23 13:26:40 +0200

    Update PSERVER.h

--- PSERVER.h
+++ PSERVER.h
@@ -9,14 +9,18 @@ public:
 	PSERVER();
 	~PSERVER();
 	void initSockets();
+	void createLisSocket();
+	void transSockAddr(in_addr* ip_addr);
 	void startServer();
 
 private:
 	void stopServer();
 private:
-	SOCKET lisSocket;
+	SOCKET lis_socket;
+	SOCKET client_socket;
 	WSADATA wsData;
 	sockaddr_in servInfo;
+	sockaddr_in clientInfo;
 	int errState;
 };
 
