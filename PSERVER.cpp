ecc78136cb6c8718a7ab4dba8415454a9e64f88b
GlebSats <133129364+GlebSats@users.noreply.github.com>
2023-05-23 13:26:04 +0200

    Update PSERVER.cpp

--- PSERVER.cpp
+++ PSERVER.cpp
@@ -1,52 +1,73 @@
 #include "PSERVER.h"
+#include "ServException.h"
 
 PSERVER::PSERVER() {
-	initSockets();
+	try
+	{
+		initSockets();
+		createLisSocket();
+		startServer();
+	}
+	catch (const ServException& ex)
+	{
+		std::cout << ex.GetErrorType() << ex.GetErrorCode() << std::endl;
+	}
 }
 
 PSERVER::~PSERVER() {
 	stopServer();
 }
 
+
 void PSERVER::initSockets()
 {
 	errState = WSAStartup(MAKEWORD(2, 2), &wsData);
 	if (errState != 0) {
-		std::cout << "Initialization version error: ";
-		std::cout << WSAGetLastError() << std::endl;
-		return;
+		throw ServException("Initialization version error: ", WSAGetLastError());
+	}
+}
+
+void PSERVER::createLisSocket()
+{
+	lis_socket = socket(AF_INET, SOCK_STREAM, 0);
+	if (lis_socket == INVALID_SOCKET) {
+		throw ServException("Socket initialization error: ", WSAGetLastError());
+	}
+
+}
+
+void PSERVER::transSockAddr(in_addr* ip_addr)
+{
+	errState = inet_pton(AF_INET, "192.168.1.104", ip_addr);
+	if (true) {
+		throw ServException("Socket address translation error");
 	}
 }
 
 void PSERVER::startServer()
 {
-	lisSocket = socket(AF_INET, SOCK_STREAM, 0);
-	if (lisSocket == INVALID_SOCKET) {
-		std::cout << "Socket initialization error: ";
-		std::cout << WSAGetLastError() << std::endl;
-		return;
-	}
-
 	in_addr ip_addr;
-	errState = inet_pton(AF_INET, "192.168.1.104", &ip_addr);
-	if (errState <= 0) {
-		std::cout << "Socket address creation error" << std::endl;
-		return;
+	try
+	{
+		transSockAddr(&ip_addr);
 	}
-
+	catch (const ServException& ex)
+	{
+		throw;
+	}
 	ZeroMemory(&servInfo, sizeof(servInfo));
 	servInfo.sin_family = AF_INET;
 	servInfo.sin_port = htons(4444);
 	servInfo.sin_addr = ip_addr;
 
-	errState = bind(lisSocket, (sockaddr*)&servInfo, sizeof(servInfo));
+	errState = bind(lis_socket, (sockaddr*)&servInfo, sizeof(servInfo));
 	if (errState != 0) {
 		std::cout << "Binding error: ";
 		std::cout << WSAGetLastError() << std::endl;
 		return;
 	}
 
-	errState = listen(lisSocket, SOMAXCONN);
+	errState = listen(lis_socket, SOMAXCONN);
 	if (errState != 0) {
 		std::cout << "Listening error: ";
 		std::cout << WSAGetLastError() << std::endl;
@@ -59,7 +80,7 @@ void PSERVER::startServer()
 
 void PSERVER::stopServer()
 {
-	closesocket(lisSocket);
+	closesocket(lis_socket);
 	WSACleanup();
 }
 
