ab908223f00a861b13d0cefda1860024f5d607c3
GlebSats <133129364+GlebSats@users.noreply.github.com>
2023-06-02 11:44:18 +0200

    Update TargetServer.h

--- TargetServer.h
+++ TargetServer.h
@@ -1,1 +1,29 @@
+#pragma once
+#include <iostream>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
 
+class TSERVER
+{
+public:
+	TSERVER() :lis_socket(-1), client_socket(-1), web_socket(-1), errState(0) {}
+	~TSERVER();
+	void startServer();
+
+private:
+	void initSockets();
+	void createNewSocket(SOCKET& new_socket);
+	void transSockAddr(const char* ip_string, in_addr* ip_addr);
+	void bindSocket();
+	void listenState();
+	void acceptConnection();
+	void connectToWebServ();
+	void sockCommunication();
+	void stopServer();
+private:
+	SOCKET lis_socket;
+	SOCKET client_socket;
+	SOCKET web_socket;
+	WSADATA wsData;
+	int errState;
+};
