eed0b70519c9ff0ac1103a40da7b3682358c7c81
GlebSats <133129364+GlebSats@users.noreply.github.com>
2023-07-02 12:10:40 +0200

    Update WebSocketProxy.cpp

--- WebSocketProxy.cpp
+++ WebSocketProxy.cpp
@@ -1,12 +1,32 @@
 #include "PSERVER.h"
+#include "ServException.h"
+#include "writeLog.h"
 #pragma comment(lib, "Ws2_32.lib")
 
 int main()
 {
-    PSERVER::serviceStopEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
-
     PSERVER server;
-    server.startServer();
-
+    server.serviceStopEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+    
+    try
+    {
+        server.serverInitialization();
+       	while (true) {
+       		try
+       		{
+                server.serverHandler();
+       		}
+       		catch (const ServException& ex)
+       		{
+       			std::string ErrorCode = std::to_string(ex.GetErrorCode());
+       			writeLog(ex.GetErrorType() + ErrorCode);
+       		}
+       	}
+    }
+    catch (const ServException& ex)
+    {
+    	std::string ErrorCode = std::to_string(ex.GetErrorCode());
+    	writeLog(ex.GetErrorType() + ErrorCode);
+    }
 }
 
