30391909441666a9c9afae32a3dd4170a0f7c007
GlebSats <133129364+GlebSats@users.noreply.github.com>
2023-07-04 07:45:48 +0200

    Update PSERVER.h

--- PSERVER.h
+++ PSERVER.h
@@ -7,8 +7,10 @@
 class PSERVER
 {
 public:
-	PSERVER() :lis_socket(-1), client_socket(-1), server_socket(-1), errState(0), bufToClientHasData(WSA_INVALID_EVENT),
-		bufToServHasData(WSA_INVALID_EVENT), clientReadySend(WSA_INVALID_EVENT), serverReadySend(WSA_INVALID_EVENT)
+	PSERVER(const char* listeningPort, const char* serverIP, const char* serverPort): 
+	listeningPort(listeningPort), serverIP(serverIP), serverPort(serverPort),lis_socket(-1), client_socket(-1),
+	server_socket(-1), errState(0), bufToClientHasData(WSA_INVALID_EVENT), bufToServHasData(WSA_INVALID_EVENT),
+	clientReadySend(WSA_INVALID_EVENT), serverReadySend(WSA_INVALID_EVENT)
 	{
 	}
 	~PSERVER();
@@ -16,11 +18,11 @@ public:
 	void serverHandler();
 
 private:
-	void initSockets();
-	void createSockInfo(const char* ip, const char* port, addrinfo** sockInfo);
-	void createNewSocket(SOCKET& new_socket, addrinfo* sockInfo);
-	void bindSocket();
-	void listenState();
+	void initSockets(); // Function initiates use of the Winsock DLL
+	void createSockInfo(const char* ip, const char* port, addrinfo** sockInfo); // Create addrinfo and translate host name to address
+	void createNewSocket(SOCKET& new_socket, addrinfo* sockInfo); // Create socket with addrinfo parameters
+	void bindSocket(); // Function associates a local address with a socket
+	void listenState(); // Set socket to listen state
 	void acceptConnection();
 	void connectToWebServ();
 	void sockCommunication();
@@ -30,6 +32,9 @@ private:
 public:
 	HANDLE serviceStopEvent;
 private:
+	const char* listeningPort;
+	const char* serverIP;
+	const char* serverPort;
 	addrinfo* lisSockInfo;
 	addrinfo* webSockInfo;
 	SOCKET lis_socket;
